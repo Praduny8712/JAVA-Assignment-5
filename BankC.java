84fa8c249524b7c15e7dace213487bfea6bcef29
Praduny Sanjay Patil <pradunyspatil@gmail.com>
2024-10-02 00:06:27 +0530

    Add files via upload

---
+++ BankC.java
@@ -0,0 +1,13 @@
+package Assignment5;
+
+public class BankC extends Bank{
+	public double getBalance(double accounybalance) {
+		return accounybalance;
+	}
+
+	@Override
+	public String toString() {
+		return "BankC [accounybalance=" + accounybalance + "]";
+	}
+	
+}
